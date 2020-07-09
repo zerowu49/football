89fac41cb1ccee789f8444899702f73562f837bb
jonathan <jonathanputra67@gmail.com>
2020-07-09 22:54:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
         navigator.serviceWorker
-            .register(`football/service-worker.js`)
+            .register(`service-worker.js`)
             .then(function () {
                 console.log("Pendaftaran ServiceWorker berhasil");
             })
